7f501d15282cc15fc1e00f00072d2ef7e84028a6
Mateusz Wiktorzak <mateusz.wiktorzak@gmail.com>
2026-05-27 23:59:51 +0200

    Fixing nmp run dev to build

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,8 +72,16 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
+    // dev
+    // command: 'npm run dev', 
+    // url: 'http://localhost:3000',
+
+    // prod
+    command: 'npm run build && npm run start', 
+    url: 'http://127.0.0.1:3000',
+
+    timeout: 180 * 1000, 
+    
     reuseExistingServer: !process.env.CI,
   },
 });
